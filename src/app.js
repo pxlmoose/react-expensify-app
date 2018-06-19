aa272c5e2069f27019af6ff8b5701880bf2d688d
Jan Nowicki <pxlmoose@gmail.com>
2018-06-19 12:42:00 +0200

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,15 +12,10 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-// store.dispatch(addExpense( {description: 'water bill', amount: 5000 }));
-// store.dispatch(addExpense( {description: 'gas bill', createdAt: 1000 }));
-// store.dispatch(addExpense( {description: 'rent', amount: 9000} ));
 
 
 const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
