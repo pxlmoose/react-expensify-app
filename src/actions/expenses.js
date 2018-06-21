1966aa0ccbe0ed579857add1d0052be488937c02
Jan Nowicki <pxlmoose@gmail.com>
2018-06-21 20:52:54 +0200

    remove expense from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,14 @@ export const removeExpense = ({ id } = {}) => ({
     id
 });
 
+export const startRemoveExpense = ({ id } = {}) => ({
+    return (dispatch) => {
+        database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+    };
+});
+
 // EDITING EXPENSES
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
